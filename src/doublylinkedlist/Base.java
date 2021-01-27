877ae4aebe3868d850a26849ed7c5f47bfa3bc48
dbijaya <dbijaythapa@gmail.com>
2021-01-27 08:03:41 +0545

    [ADD] doubly linked-list package added.

---
+++ src/doublylinkedlist/Base.java
@@ -0,0 +1,22 @@
+package doublylinkedlist;
+
+public class Base {
+
+	public static void main(String[] args) {
+		
+		Employee bishalUpadhyaya = new Employee("Bishal", "Upadhyaya", 1);
+		Employee sanjeevRoka = new Employee("Sanjeev", "Roka", 2);
+		Employee ramThapa = new Employee("Ram", "Thapa", 3);
+		Employee sitaBasnet = new Employee("Sita", "Basnet", 4);
+		
+		EmployeeDoublyLinkedList list = new EmployeeDoublyLinkedList();
+		list.addToFront(bishalUpadhyaya);
+		list.addToFront(sanjeevRoka);
+		list.addToFront(ramThapa);
+		list.addToFront(sitaBasnet);
+		
+		list.printList();
+
+	}
+
+}
---
+++ src/doublylinkedlist/EmployeeDoublyLinkedList.java
@@ -0,0 +1,25 @@
+package doublylinkedlist;
+
+public class EmployeeDoublyLinkedList {
+	
+	private EmployeeNode head;
+	
+	public void addToFront(Employee employee) {
+		EmployeeNode node =  new EmployeeNode(employee);
+		node.setNext(head);
+		head = node;
+	}
+	
+	public void printList() {
+		EmployeeNode current = head;
+		System.out.print("HEAD -> ");
+		while (current != null) {
+			System.out.print(current);
+			System.out.println(" -> ");
+			current = current.getNext();
+			
+		}
+		System.out.println("null");
+	}
+
+}
