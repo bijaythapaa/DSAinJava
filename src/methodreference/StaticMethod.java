c3d7ef6a3f66afcddf21b301e56c71af7d484bb3
dbijaya <dbijaythapa@gmail.com>
2021-03-03 14:30:06 +0545

    [ADD] method reference practise

---
+++ src/methodreference/StaticMethod.java
@@ -0,0 +1,12 @@
+package methodreference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StaticMethod {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+//        boolean isReal = list.stream().anyMatch(u -> User.isRealUser(u));
+    }
+    
+}
