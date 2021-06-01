4dbad7ab379fa64820224c69d9eed5127401d781
dbijaya <dbijaythapa@gmail.com>
2021-06-01 21:29:12 +0545

    vector is thread-safe arraylist.

--- src/vectordemo/Base.java
+++ src/vectordemo/Base.java
@@ -4,18 +4,16 @@ import java.util.List;
 import java.util.Vector;
 
 public class Base {
-	
-	public static void main(String[] args) {
-		List<Employee> employeeList = new Vector<>();
-		employeeList.add(new Employee("Bishal", "Upadhyaya", 1));
-		employeeList.add(new Employee("Santosh", "Paneru", 2));
-		employeeList.add(new Employee("Sanjeev", "Roka", 3));
-		employeeList.add(new Employee("Vijay", "Dhakal", 4));
+
+    public static void main(String[] args) {
+        //    vectors are essentially the thread-safe ArrayList.
+        List<Employee> employeeList = new Vector<>();
+        employeeList.add(new Employee("Bishal", "Upadhyaya", 1));
+        employeeList.add(new Employee("Santosh", "Paneru", 2));
+        employeeList.add(new Employee("Sanjeev", "Roka", 3));
+        employeeList.add(new Employee("Vijay", "Dhakal", 4));
 
 //		employeeList.forEach(employee -> System.out.println(employee));
-		employeeList.forEach(System.out::println);
-
-		
-	}
-
+        employeeList.forEach(System.out::println);
+    }
 }
