230284e29305f563b2f66643709f1e8d7e604a98
dbijaya <dbijaythapa@gmail.com>
2021-06-01 21:28:34 +0545

    singly-linkedlist getSize, removeFromFront, isEmpty methods added.

--- src/singlylinkedlist/Base.java
+++ src/singlylinkedlist/Base.java
@@ -2,21 +2,23 @@ package singlylinkedlist;
 
 public class Base {
 
-	public static void main(String[] args) {
-		
-		Employee bishalUpadhyaya = new Employee("Bishal", "Upadhyaya", 1);
-		Employee sanjeevRoka = new Employee("Sanjeev", "Roka", 2);
-		Employee ramThapa = new Employee("Ram", "Thapa", 3);
-		Employee sitaBasnet = new Employee("Sita", "Basnet", 4);
-		
-		EmployeeLinkedList list = new EmployeeLinkedList();
-		list.addToFront(bishalUpadhyaya);
-		list.addToFront(sanjeevRoka);
-		list.addToFront(ramThapa);
-		list.addToFront(sitaBasnet);
-		
-		list.printList();
+    public static void main(String[] args) {
 
-	}
+        Employee bishalUpadhyaya = new Employee("Bishal", "Upadhyaya", 1);
+        Employee sanjeevRoka = new Employee("Sanjeev", "Roka", 2);
+        Employee ramThapa = new Employee("Ram", "Thapa", 3);
+        Employee sitaBasnet = new Employee("Sita", "Basnet", 4);
 
+        EmployeeLinkedList list = new EmployeeLinkedList();
+        System.out.println(list.isEmpty());
+        list.addToFront(bishalUpadhyaya);
+        list.addToFront(sanjeevRoka);
+        list.addToFront(ramThapa);
+        list.addToFront(sitaBasnet);
+        System.out.println(list.getSize());
+        list.printList();
+        list.removeFromFront();
+        System.out.println(list.getSize());
+        list.printList();
+    }
 }
--- src/singlylinkedlist/EmployeeLinkedList.java
+++ src/singlylinkedlist/EmployeeLinkedList.java
@@ -1,25 +1,43 @@
 package singlylinkedlist;
 
 public class EmployeeLinkedList {
-	
-	private EmployeeNode head;
-	
-	public void addToFront(Employee employee) {
-		EmployeeNode node =  new EmployeeNode(employee);
-		node.setNext(head);
-		head = node;
-	}
-	
-	public void printList() {
-		EmployeeNode current = head;
-		System.out.print("HEAD -> ");
-		while (current != null) {
-			System.out.print(current);
-			System.out.println(" -> ");
-			current = current.getNext();
-			
-		}
-		System.out.println("null");
-	}
 
+    private EmployeeNode head;
+    private int size;
+
+    public void addToFront(Employee employee) {
+        EmployeeNode node = new EmployeeNode(employee);
+        node.setNext(head);
+        head = node;
+        size++;
+    }
+
+    public EmployeeNode removeFromFront() {
+        if (isEmpty()) return null;
+        EmployeeNode removedNode = head;
+        head = head.getNext();
+        size--;
+        removedNode.setNext(null);
+        return removedNode;
+    }
+
+    public void printList() {
+        EmployeeNode current = head;
+        System.out.print("HEAD -> ");
+        while (current != null) {
+            System.out.print(current);
+            System.out.println(" -> ");
+            current = current.getNext();
+
+        }
+        System.out.println("null");
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
 }
