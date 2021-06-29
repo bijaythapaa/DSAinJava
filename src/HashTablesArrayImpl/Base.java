56a1b4437464e9be1239278940ebf5d4f47fd597
dbijaya <dbijaythapa@gmail.com>
2021-06-29 07:27:10 +0545

    HashTable Array Implementation.

---
+++ src/HashTablesArrayImpl/Base.java
@@ -0,0 +1,28 @@
+package HashTablesArrayImpl;
+
+
+/**
+ * @Author Bijay Thapa
+ * @Project DSAinJava
+ * @created 6/29/21 - 7:01 AM
+ */
+public class Base {
+    public static void main(String[] args) {
+        Employee shyam = new Employee("Shyam", "Khan", 1);
+        Employee sanjeev = new Employee("Sanjeev", "Singh", 2);
+        Employee ram = new Employee("Ram", "Kumar", 3);
+        Employee sita = new Employee("Sita", "Dev", 4);
+        Employee hari = new Employee("Hari", "Chandra", 5);
+
+        SimpleHashTable ht = new SimpleHashTable();
+        ht.put("Khan", shyam);
+        ht.put("Singh", sanjeev);
+        ht.put("Chandra", hari);
+        ht.put("Dev", sita);
+        ht.put("Kumar", ram);
+
+//        ht.printHashTable();
+
+        System.out.println("Retrive key Chandra: " + ht.get("Chandra"));
+    }
+}
---
+++ src/HashTablesArrayImpl/SimpleHashTable.java
@@ -0,0 +1,43 @@
+package HashTablesArrayImpl;
+
+/**
+ * @Author Bijay Thapa
+ * @Project DSAinJava
+ * @created 6/29/21 - 7:04 AM
+ */
+public class SimpleHashTable {
+
+    private Employee[] hashtable;
+
+    public SimpleHashTable() {
+        hashtable = new Employee[10];
+    }
+
+    public void put(String key, Employee employee) {
+        int hashedKey = hashKey(key);
+        if (hashtable[hashedKey] != null)
+            System.out.println("Sorry there's already an employee at position " + hashedKey);
+        else
+            hashtable[hashedKey] = employee;
+    }
+
+    public Employee get(String key) {
+        int hashedKey = hashKey(key);
+        return hashtable[hashedKey];
+    }
+
+    public void printHashTable() {
+//        for (int i = 0; i < hashtable.length; i++) {
+//            System.out.println(hashtable[i]);
+//        }
+
+//        Enhanced For loop.
+        for (Employee employee : hashtable) {
+            System.out.println(employee);
+        }
+    }
+
+    private int hashKey(String key) {
+        return key.length() % hashtable.length;
+    }
+}
