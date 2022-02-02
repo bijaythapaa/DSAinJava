254c5f5e92e64c4f50a2430dfdc4629b46016810
dbijaya <dbijaythapa@gmail.com>
2022-02-02 22:05:25 +0545

    iterations and loops.

--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -21,28 +21,48 @@ public class ShopApp {
         item2.price = 10.5;
         item2.size = "S";
 
-        Clothing[] clothing = {item1, item2};
+        Clothing[] clothing = {item1, item2, new Clothing(), new Clothing()};
+        clothing[2].description = "Green Scarf";
+        clothing[2].price = 5.0;
+        clothing[2].size = "S";
+        clothing[3].description = "Blue T-Shirt";
+        clothing[3].price = 10.5;
+        clothing[3].size = "S";
 
 
-        System.out.println("item1= Description: " + item1.description + ", Price: " + item1.price + ", Size: " + item1.size);
-        System.out.println("item2= Description: " + item2.description + ", Price: " + item2.price + ", Size: " + item2.size);
-        total = (2 * item2.price + item1.price) * (1 + tax);
+//        System.out.println("item1= Description: " + item1.description + ", Price: " + item1.price + ", Size: " + item1.size);
+//        System.out.println("item2= Description: " + item2.description + ", Price: " + item2.price + ", Size: " + item2.size);
+//        total = (2 * item2.price + item1.price) * (1 + tax);
 //        total += total * tax;
-        System.out.println("Total Amount: " + total);
 
         int measurement = 3;
         switch (measurement) {
-            case 1: case 2: case 3:
+            case 1:
+            case 2:
+            case 3:
                 c1.size = "S";
                 break;
-            case 4: case 5: case 6:
+            case 4:
+            case 5:
+            case 6:
                 c1.size = "M";
                 break;
-            case 7: case 8: case 9:
+            case 7:
+            case 8:
+            case 9:
                 c1.size = "L";
                 break;
             default:
                 c1.size = "X";
+        }
+
+        for (Clothing item : clothing) {
+            if (c1.size.equals(item.size)) {
+                total += total + item.price;
+                System.out.println("Item = Description: " + item.description + ", Price: " + item.price + ", Size: " + item.size);
+                if (total > 15) break;
+            }
+            System.out.println("Total = " + total);
         }
     }
 }
