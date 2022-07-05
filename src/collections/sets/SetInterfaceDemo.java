786145254df3c318e4fe3602d3b06204b463db4b
bijaythapaa <dbijaythapa@gmail.com>
2022-07-05 16:43:41 +0545

    Set Interface

---
+++ src/collections/sets/SetInterfaceDemo.java
@@ -0,0 +1,14 @@
+package collections.sets;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class SetInterfaceDemo {
+    public static void main(String[] args) {
+        List<String> strings = List.of("one", "two", "three", "four", "five", "six");
+        Set<String> set = new HashSet<>();
+        set.addAll(strings);
+        set.forEach(System.out::println);
+    }
+}
