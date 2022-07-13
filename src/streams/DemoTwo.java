529bcb66314adabb4c67747edac5b64d32a7a850
bijaythapaa <dbijaythapa@gmail.com>
2022-07-13 22:56:40 +0545

    streams DemoTwo

---
+++ src/streams/DemoTwo.java
@@ -0,0 +1,11 @@
+package streams;
+
+import java.util.List;
+
+public class DemoTwo {
+    public static void main(String[] args) {
+        List<String> strings = List.of("one", "two", "three", "four", "five");
+//        var map = strings.stream().collect(groupingBy(String::length, counting()));
+//        map.forEach(key, value) ->System.out.println(key + " :: " + value);*/
+    }
+}
