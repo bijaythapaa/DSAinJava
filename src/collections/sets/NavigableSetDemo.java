945fa2d7160e8de4a49a0da99106d0c4e62a1dcf
bijaythapaa <dbijaythapa@gmail.com>
2022-09-07 22:57:30 +0545

    about Navigable sets

--- src/collections/sets/NavigableSetDemo.java
+++ src/collections/sets/NavigableSetDemo.java
@@ -4,6 +4,12 @@ import java.util.NavigableSet;
 import java.util.Set;
 import java.util.TreeSet;
 
+/*
+* Java SE 6 saw the introduction of an extension of SortedSet with the addition of more methods.
+* It turns out that the TreeSet class was retrofitted to implement NavigableSet.
+* So you can use the same class for both interfaces.
+*/
+
 public class NavigableSetDemo {
     public static void main(String[] args) {
         NavigableSet<String> sortedStrings = new TreeSet<>(Set.of("a", "b", "c", "d", "e", "f"));
