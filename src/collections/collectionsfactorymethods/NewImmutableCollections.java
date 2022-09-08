c0b131380c46861f1cd2b61a1cd1840cb82b20f2
bijaythapaa <dbijaythapa@gmail.com>
2022-09-08 08:40:10 +0545

    creating new immutable collections

---
+++ src/collections/collectionsfactorymethods/NewImmutableCollections.java
@@ -0,0 +1,11 @@
+package collections.collectionsfactorymethods;
+
+import java.util.List;
+import java.util.Set;
+
+public class NewImmutableCollections {
+    List<String> stringList = List.of("one", "two", "three");
+    Set<String> stringSet = Set.of("one", "two", "three");
+
+    // these .of() methods are commonly referred to as 'convenience factory methods for collections
+}
