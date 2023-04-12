8eacf0cb222175183af203c3421bd727f93fb9f0
bijaythapaa <dbijaythapa@gmail.com>
2023-04-12 23:49:50 +0545

    Reverse number via recursion.

--- src/recursion/RecursiveNumberReverse.java
+++ src/recursion/RecursiveNumberReverse.java
@@ -11,6 +11,7 @@ public class RecursiveNumberReverse {
     public static void main(String[] args) {
         reverse(2143);
         System.out.println("Reverse of the given number is : " + sum);
+        System.out.println("Reverse of the giver number is : " + reverseTwo(314265));
     }
 
     private static void reverse(int number) {
@@ -18,5 +19,18 @@ public class RecursiveNumberReverse {
         int rem = number % 10;
         sum = sum * 10 + rem;
         reverse(number / 10);
+    }
+
+    private static int reverseTwo(int num) {
+        // sometimes we might need some additional argument to the function
+        // in that case, make a new helper function.
+        int digits = (int) (Math.log10(num)) + 1;
+        return helper(num, digits);
+    }
+
+    private static int helper(int num, int digits) {
+        if (num % 10 == num) return num;
+        int rem = num % 10;
+        return rem * (int) (Math.pow(10, digits - 1)) + helper(num / 10, digits - 1);
     }
 }
