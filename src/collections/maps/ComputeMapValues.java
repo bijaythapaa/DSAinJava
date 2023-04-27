1cc726c405562963a7920e57badac8fbe74f9840
bijaythapaa <dbijaythapa@gmail.com>
2023-04-28 00:08:19 +0545

    Manipulate N*3 and N*2 metrics

--- src/collections/maps/ComputeMapValues.java
+++ src/collections/maps/ComputeMapValues.java
@@ -1,9 +1,7 @@
 package collections.maps;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author : bijay.thapa
@@ -44,5 +42,56 @@ public class ComputeMapValues {
         strings.forEach(word -> mapFromMerge.merge(word.length(), word,
                 (existingValue, newWord) -> existingValue + ", " + newWord));
         mapFromMerge.forEach((key, value) -> System.out.println(key + " :: " + value));
+
+        /* Manipulating (N*3) metrics */
+        String[][] tableNx3 = {
+                {"a", "ab", "abc"},
+                {"a", "ab", "bcd"},
+                {"a", "bc", "cdf"},
+                {"b", "ab", "abc"},
+                {"b", "ce", "def"}
+        };
+        /* Solution One */
+        Map<String, Map<String, Set<String>>> mapMapOne = new HashMap<>();
+        Arrays.asList(tableNx3).forEach(row -> mapMapOne.computeIfAbsent(row[0], k -> new HashMap<>())
+                .computeIfAbsent(row[1], k -> new HashSet<>())
+                .add(row[2]));
+        System.out.println(mapMapOne);
+
+        /* Solution Two */
+        Map<String, Map<String, Set<String>>> mapMapTwo = new HashMap<>();
+        Arrays.asList(tableNx3).forEach(row -> mapMapTwo.computeIfAbsent(row[0], k -> new HashMap<>())
+                .computeIfAbsent(row[1], k -> new HashSet<>())
+                .add(row[2]));
+        System.out.println(mapMapTwo);
+
+        /* Solution Three */
+        Map<String, Map<String, Set<String>>> mapMapThree = new HashMap<>();
+
+        Arrays.asList(tableNx3).forEach(row -> mapMapThree.computeIfAbsent(row[0], k -> new HashMap<>())
+                .computeIfAbsent(row[1], k -> Arrays.stream(new String[]{row[2]}).collect(Collectors.toSet()))
+                .add(row[2]));
+        System.out.println(mapMapThree);
+
+        /* Manipulating (N*2) metrics */
+        String[][] tableNx2 = {
+                {"a", "ab"},
+                {"a", "bc"},
+                {"a", "cd"},
+                {"b", "ab"},
+                {"b", "ce"},
+                {"b", "bc"},
+                {"c", "ab"},
+                {"c", "ef"}
+        };
+        /* Solution One */
+        Map<String, List<String>> resultOne = Arrays.stream(tableNx2).collect(Collectors.groupingBy(row -> row[0],
+                Collectors.mapping(row -> row[1], Collectors.toList())));
+        System.out.println(resultOne);
+
+        /* Solution Two */
+        Map<String, Set<String>> resultTwo = Arrays.stream(tableNx2).collect(Collectors.groupingBy(row -> row[0],
+                Collectors.mapping(row -> row[1], Collectors.toSet())));
+        System.out.println(resultTwo);
     }
 }
