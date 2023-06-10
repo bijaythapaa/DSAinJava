c20a97a97dbf438f1fb544b17fdd4f65d4ae9093
bijaythapaa <dbijaythapa@gmail.com>
2023-06-10 17:01:49 +0545

    Guava Immutables

---
+++ src/immutables/GuavaImmutables.java
@@ -0,0 +1,35 @@
+package immutables;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author : bijay.thapa
+ * @created : 6/10/23, Saturday
+ * @Description :
+ **/
+public class GuavaImmutables {
+    /*
+     * An ImmutableList object does not allow null elements. If any attempt then NullPointer exception will be thrown.
+     *
+     * Class Hierarchy:
+     * Object -> AbstractCollection -> ImmutableCollection -> ImmutableList
+     * */
+
+    public static void main(String[] args) {
+        ImmutableList immutableList = ImmutableList.of("Immutable", "Lists", "Guava");
+        System.out.println("immutableList = " + immutableList);
+
+        List list = new ArrayList<>(Arrays.asList("Using", "CopyOf", "Method"));
+        ImmutableList immutableList1 = ImmutableList.copyOf(list);
+        System.out.println("ImmutableList using copyOf() method" + ":" + immutableList1);
+
+        List list1 = new ArrayList<>(Arrays.asList("Using", "CopyOf", "Method", null));
+        ImmutableList immutableList2 = ImmutableList.copyOf(list1);
+        System.out.println("ImmutableList to insert null element" + ":" + immutableList2);
+
+        ImmutableList iList = ImmutableList.builder().add("using", "builder", "method").build()
+        System.out.println("iList = " + iList);
+    }
+}
